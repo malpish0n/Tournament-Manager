34d164b7c89a669a255a94b6772be16d09336632
malpish0n <akasztelan617@gmail.com>
2026-01-22 10:18:36 +0100

    Initial commit

---
+++ src/test/java/com/esport/tournamentmanager/TournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esport.tournamentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
